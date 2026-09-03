b09eeac5ea21e04cee8bb3157bcb199ca013e94c
jovannyreyes-23 <jovannyreyes.23@csu.fullerton.edu>
2026-09-03 08:48:41 -0700

    finished assignment 4

---
+++ M1A4.py
@@ -0,0 +1,10 @@
+# Name: Jovanny Reyes
+# Student ID: 887699056
+# Section: 18254
+# Assignment: Module 1 Assignment 4
+
+catchphrase_str = input("Please enter a catchphrase: ")
+print(f"[{catchphrase_str.upper()}]")
+print(f"[{catchphrase_str.lower()}]")
+print(f"[{catchphrase_str.lstrip()}]")
+print(f"[{catchphrase_str.rstrip()}]")
