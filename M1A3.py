cea3be565a081583c63772d63fa904b78149a464
jovannyreyes-23 <jovannyreyes.23@csu.fullerton.edu>
2026-09-03 09:02:21 -0700

    finished assignment 3

---
+++ M1A3.py
@@ -0,0 +1,19 @@
+# Name: Jovanny Reyes
+# Student ID: 887699056
+# Section: 18254
+# Assignment: Module 1 Assignment 3
+
+first_int = int(input("What is the integer? "))
+second_float = float(input("What is the float? "))
+
+message_str = (f"{first_int} + {second_float} is {first_int + second_float}")
+print(message_str)
+
+message_str = (f"{first_int} - {second_float} is {first_int - second_float}")
+print(message_str)
+
+message_str = (f"{first_int} * {second_float} is {first_int * second_float}")
+print(message_str)
+
+message_str = (f"{first_int} / {second_float} is {first_int / second_float}")
+print(message_str)
