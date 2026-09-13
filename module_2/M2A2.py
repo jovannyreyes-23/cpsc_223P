585f16a6e292246474ff9d96e5249f57c8b27d11
jovannyreyes-23 <jovannyreyes.23@csu.fullerton.edu>
2026-09-13 13:57:09 -0700

    typo

--- module_2/M2A2.py
+++ module_2/M2A2.py
@@ -3,7 +3,7 @@
 # Section: 18254
 # Assignment: Module 2 Assignment 2
 
-g_list = ["Mortal Kombat", "Contra", "Sheets Of Rage", "Shinobi", "Sonic", "Phantasy Star"]
+g_list = ["Mortal Kombat", "Contra", "Streets Of Rage", "Shinobi", "Sonic", "Phantasy Star"]
 print("Here are the top Sega games: ")
 
 for item in g_list:
