ffb3294ede539878455659f70c920ce6d3def00f
jovannyreyes-23 <jovannyreyes.23@csu.fullerton.edu>
2026-09-17 09:20:07 -0700

    Prompt the user for an integer, the print out if it is odd or even.

---
+++ module_3/M3A3.py
@@ -0,0 +1,11 @@
+# Name: Jovanny Reyes
+# Student ID: 887699056
+# Section: 18254
+# Assignment: Module 3 Assignment 3
+
+user_int = int(input("Please enter an integer: "))
+
+if user_int % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
