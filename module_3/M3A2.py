259668ca03af348ad535f69515534e912055e722
jovannyreyes-23 <jovannyreyes.23@csu.fullerton.edu>
2026-09-17 09:18:49 -0700

    Program that starts with a list of movies, prompts a user to enter their favorite movie, and reacts differently whether or not it's in the list.

---
+++ module_3/M3A2.py
@@ -0,0 +1,13 @@
+# Name: Jovanny Reyes
+# Student ID: 887699056
+# Section: 18254
+# Assignment: Module 3 Assignment 2
+
+m_list = ["E.T. the Extra-Terrestrial", "Indiana Jones and the Last Crusade", "Batman", "Rain Man", "Back to the Future Part II"]
+
+user_movie_str = input("What is your favorite movie? ")
+
+if user_movie_str in m_list:
+    print("Good choice")
+else:
+    print("Bad choice")
